98270ac7d14536188f65c7b85099c826c6753c48
chiragrana25 <66072833+chiragrana25@users.noreply.github.com>
2026-06-03 15:32:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import yfinance as yf
 from playwright.sync_api import sync_playwright
 
 WEBAPP_URL = os.environ.get('WEBAPP_URL')
-TICKERS = ['SPX','NVDA', 'SPY', 'QQQ', 'MU', 'SNDK', 'WDC', 'AAPL', 'AMD', 'CRWV', 'NBIS', 'MSFT', 'QCOM', 'AAOI', 'ASTS', 'RDDT', 'ALAB', 'ANET','TSLA', 'RKLB','ORCL', 'AMZN']
+TICKERS = ['SPX','NVDA', 'SPY', 'QQQ', 'MU', 'SNDK', 'WDC', 'AAPL', 'AMD', 'CRWV', 'NBIS', 'MSFT', 'QCOM', 'AAOI', 'ASTS', 'RDDT', 'ALAB', 'ANET','TSLA', 'RKLB','ORCL', 'AMZN', 'MSTR']
 
 def rgb_to_hex(rgb_str):
     """Hardened conversion: Ignores transparency and handles empty values."""
